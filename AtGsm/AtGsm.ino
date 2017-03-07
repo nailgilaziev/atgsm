da51ac3073f488e93cb7d9ec6711bec34151ac4a
Nail Gilaziev <nail.gilaziev@gmail.com>
2017-03-07 17:31:43 +0300

    Initial Commit

---
+++ AtGsm/AtGsm.ino
@@ -0,0 +1,64 @@
+//
+// AtGsm
+//
+// Gsm over at commands
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Nail
+// 				Nail
+//
+// Date			07/03/2017 17:31
+// Version		<#version#>
+//
+// Copyright	© Nail, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ AtGsm/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  AtGsm
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project AtGsm
+  Created by Nail on 07/03/2017
+  Copyright © 2017 Nail
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
